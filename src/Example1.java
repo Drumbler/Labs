736777b908145d489a34115b7d25e41111f4ffe2
Andrey <andy132012@gmail.com>
2022-11-30 20:03:40 +0500

    Initial commit

---
+++ src/Example1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Введите вашу фамилию");
+        String fam = scan.nextLine();
+
+        System.out.println("Теперь введите имя");
+        String fName = scan.nextLine();
+
+        System.out.println("И наконец ваше отчество");
+        String sName = scan.nextLine();
+
+        System.out.println("hello " + fam + " " + fName + " " + sName);
+
+
+    }
+
+
+}
