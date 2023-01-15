1004b07cab732e57c4ea3a6b9ddd04250a484761
Andrey <andy132012@gmail.com>
2023-01-15 23:40:58 +0500

    Лабы

--- src/LR7/Example1/SuperClass.java
+++ src/LR7/Example1/SuperClass.java
@@ -1,10 +1,9 @@
 package LR7.Example1;
 
 public class SuperClass {
-    //приватное текстовое поле
+
     private String str1;
 
-    //конструктор с текстовым аргументом
     SuperClass(String strEx) {
         this.str1 = strEx;
     }
@@ -13,7 +12,7 @@ public class SuperClass {
         this.str1 = "нет параметров";
     }
 
-    //@Override - аннотация указываемая для того, чтобы показать что далее мы собираемся переопределять метод суперкласса
+
     @Override
     public String toString() {
         String superClassNameAndFieldValue;
